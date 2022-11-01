7206d412c2f64370c3856a6784d551890063d2dd
stromez <zemorts@gmail.com>
2022-11-01 12:56:17 +0700

    so sau lon hon trc, trc lon hon sau

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function toHop(array, size, khongTrung) {
+function toHop(array, size, khongTrung, sauLonHonTruoc, truocLonHonSau) {
     function iter(parts) {
         return function (v) {
             var temp = parts.concat(v);
@@ -13,8 +13,20 @@ function toHop(array, size, khongTrung) {
                 }
             }
 
-            //* NEU NHU M THEM DIEU KIEN THI VIET VAO DAY
+            //* NEU NHU M THEM DIEU KIEN THI VIET VAO DAY, DUNG CAI TEMP DE SO SANH
+            if (sauLonHonTruoc) {
+                if (!checkSauLonHonTruoc(temp)) {
+                    return
+                }
+            }
 
+            if (truocLonHonSau) {
+                if (!checkTruocLonHonSau(temp)) {
+                    return
+                }
+            }
+            
+            // ket thuc so sanh, push ket qua
             if (temp.length === size) {
                 result.push(temp);
                 return;
@@ -34,16 +46,38 @@ function start() {
     var inputArray = input.split('').map(i => Number(i))
     var size = $('#size').val() * 1
     var khongTrung = $('#khongTrung').is(":checked")
+    var sauLonHonTruoc = $('#sauLonHonTruoc').is(":checked")
+    var truocLonHonSau = $('#truocLonHonSau').is(":checked")
 
     if ($('#chiaHet').is(":checked")) {
-        var resultTemp = toHop(inputArray, size, khongTrung).map(a => a.join(''))
+        var resultTemp = toHop(inputArray, size, khongTrung, sauLonHonTruoc, truocLonHonSau).map(a => a.join(''))
         var result = resultTemp.filter(num => num % $('#chiaHetCho').val() == 0);
         $('#resultTong').val(result.length)
         $('#result').val(result)
     } else {
-        var result = toHop(inputArray, size, khongTrung).map(a => a.join(''))
+        var result = toHop(inputArray, size, khongTrung, sauLonHonTruoc, truocLonHonSau).map(a => a.join(''))
         $('#resultTong').val(result.length)
         $('#result').val(result)
     }
 
 }
+
+function checkSauLonHonTruoc(array) {
+    var length = array.length;
+    return array.every(function (value, index) {
+        var nextIndex = index + 1;
+        return nextIndex < length ? value <= array[nextIndex] : true;
+    });
+}
+
+function checkTruocLonHonSau(array) {
+    var length = array.length;
+    return array.every(function (value, index) {
+        var nextIndex = index + 1;
+        return nextIndex < length ? value >= array[nextIndex] : true;
+    });
+}
+
+
+// console.log(checkTruocLonHonSau([5, 4, 3, 4, 1]));
+// console.log(checkSauLonHonTruoc([1, 2, 3, 4, 5]));
